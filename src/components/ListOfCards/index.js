d0042d00296a1ade7f688fc1d26796e8b6bcfca3
sampxcs <ianrosales634@gmail.com>
2022-08-23 10:29:40 -0300

    last commit

--- src/components/ListOfCards/index.js
+++ src/components/ListOfCards/index.js
@@ -1,71 +1,28 @@
 import React, { useEffect, useState } from 'react'
 import './style.css'
 
-import useUser from '../../hooks/useUser'
 import Card from '../Card'
 
 export default function ListOfCards({ globalPexels }) {
-  const { user, userStatusCode } = useUser()
-  const [ready, setReady] = useState(false)
-
-  useEffect(() => {
-    if (userStatusCode === 2 && globalPexels.length)
-      globalPexels.forEach((pexel) => {
-        user.savedPexels.forEach((doc) => {
-          if (doc.id === pexel.id) {
-            pexel.isSaved = doc.id
-
-            setReady(true)
-          }
-        })
-      })
-  }, [userStatusCode])
-
-  if (userStatusCode === 1) return null
-
-  if (userStatusCode === 2 && ready)
-    return (
-      <div className='list-of-cards'>
-        {user.savedPexels && globalPexels.length
-          ? globalPexels.map(({ id, src, photographer, photographer_url, alt, avg_color, isSaved }) => (
-              <>
-                <Card
-                  className={'card-main'}
-                  key={id}
-                  id={id}
-                  src={src}
-                  photographer={photographer}
-                  photographer_url={photographer_url}
-                  alt={alt}
-                  avg_color={avg_color}
-                  isSaved={isSaved}
-                />
-              </>
-            ))
-          : null}
-      </div>
-    )
-
-  if (userStatusCode === 3)
-    return (
-      <div className='list-of-cards'>
-        {globalPexels.length
-          ? globalPexels.map(({ id, src, photographer, photographer_url, alt, avg_color, isSaved }) => (
-              <>
-                <Card
-                  className={'card-main'}
-                  key={id}
-                  id={id}
-                  src={src}
-                  photographer={photographer}
-                  photographer_url={photographer_url}
-                  alt={alt}
-                  avg_color={avg_color}
-                  isSaved={isSaved}
-                />
-              </>
-            ))
-          : null}
-      </div>
-    )
+  return (
+    <div className="list-of-cards">
+      {globalPexels.length
+        ? globalPexels.map(({ id, src, photographer, photographer_url, alt, avg_color, isSaved }) => (
+            <>
+              <Card
+                className={'card-main'}
+                key={id}
+                id={id}
+                src={src}
+                photographer={photographer}
+                photographer_url={photographer_url}
+                alt={alt}
+                avg_color={avg_color}
+                isSaved={isSaved}
+              />
+            </>
+          ))
+        : null}
+    </div>
+  )
 }
--- src/components/PexelsResults/index.js
+++ src/components/PexelsResults/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useCallback } from 'react'
+import React, { useEffect, useCallback, useState } from 'react'
 import { Redirect } from 'wouter'
 import { Helmet } from 'react-helmet'
 
@@ -9,11 +9,15 @@ import useNearScreen from '../../hooks/useNearScreen'
 import ListOfCards from '../ListOfCards'
 import GridPlaceholder from '../Placeholders/GridPlaceholder'
 import Spinner from '../Spinner'
+import useUser from '../../hooks/useUser'
 
 function PexelsResults({ params }) {
   let { keyword } = params
   if (!keyword) keyword = 'Nature'
+
   const { loading, error, globalPexels, page, setPage } = usePexels({ keyword })
+  const { user, userStatusCode } = useUser()
+  //const [ready, setReady] = useState(false)
 
   const { isNearScreen, ref } = useNearScreen({
     distance: '400px',
@@ -29,12 +33,25 @@ function PexelsResults({ params }) {
   )
 
   useEffect(() => {
+    //setReady(false)
+    if (userStatusCode === 2 && globalPexels.length) {
+      globalPexels.forEach((pexel) => {
+        user.savedPexels.forEach((doc) => {
+          if (doc.id === pexel.id) {
+            pexel.isSaved = doc.id
+          }
+        })
+      })
+    }
+  }, [userStatusCode, isNearScreen])
+
+  useEffect(() => {
     isNearScreen && handleNextPage()
   }, [handleNextPage, isNearScreen])
 
-  if (error) return <Redirect to='/404' />
+  if (error) return <Redirect to="/404" />
 
-  if (loading && page === 1) {
+  if (userStatusCode === 1 || (loading && page === 1)) {
     return (
       <>
         <Helmet>
@@ -52,14 +69,14 @@ function PexelsResults({ params }) {
           <Helmet>
             <title>Nature</title>
             <meta
-              name='description'
-              content='Free photos and videos of nature that you can use wherever you want. Search millions of high-quality, royalty-free images of beautiful nature. It is not necessary to mention the source.'
+              name="description"
+              content="Free photos and videos of nature that you can use wherever you want. Search millions of high-quality, royalty-free images of beautiful nature. It is not necessary to mention the source."
             />
           </Helmet>
         ) : (
           <Helmet>
             <title>{`Nature | ${decodeURI(keyword)}`}</title>
-            <meta name='description' content={`Search result of images and videos of ${keyword}`} />
+            <meta name="description" content={`Search result of images and videos of ${keyword}`} />
           </Helmet>
         )}
         <ListOfCards globalPexels={globalPexels} />
@@ -68,25 +85,26 @@ function PexelsResults({ params }) {
     )
   }
 
-  return (
-    <>
-      {keyword === 'Nature' ? (
-        <Helmet>
-          <title>Nature</title>
-          <meta
-            name='description'
-            content='Free photos and videos of nature that you can use wherever you want. Search millions of high-quality, royalty-free images of beautiful nature. It is not necessary to mention the source.'
-          />
-        </Helmet>
-      ) : (
-        <Helmet>
-          <title>{`Nature | ${decodeURI(keyword)}`}</title>
-          <meta name='description' content={`Search result of images and videos of ${keyword}`} />
-        </Helmet>
-      )}
-      <ListOfCards globalPexels={globalPexels} />
-      <div id='visor' ref={ref}></div>
-    </>
-  )
+  if (userStatusCode === 2)
+    return (
+      <>
+        {keyword === 'Nature' ? (
+          <Helmet>
+            <title>Nature</title>
+            <meta
+              name="description"
+              content="Free photos and videos of nature that you can use wherever you want. Search millions of high-quality, royalty-free images of beautiful nature. It is not necessary to mention the source."
+            />
+          </Helmet>
+        ) : (
+          <Helmet>
+            <title>{`Nature | ${decodeURI(keyword)}`}</title>
+            <meta name="description" content={`Search result of images and videos of ${keyword}`} />
+          </Helmet>
+        )}
+        <ListOfCards globalPexels={globalPexels} />
+        <div id="visor" ref={ref}></div>
+      </>
+    )
 }
 export default React.memo(PexelsResults)
